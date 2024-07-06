0c140ef9a1a1167c3a60029ba80a4a5fb784ad95
Uriel Sarmiento <uriel.sarmiento.solano@gmail.com>
2024-07-06 13:00:51 -0600

    add CrossOrigin annotation to EmployeeController

--- src/main/java/com/uriel/springboot/ems_backend/ems_backend/controller/EmployeeController.java
+++ src/main/java/com/uriel/springboot/ems_backend/ems_backend/controller/EmployeeController.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ import com.uriel.springboot.ems_backend.ems_backend.service.EmployeeService;
 
 import lombok.AllArgsConstructor;
 
+@CrossOrigin("*")
 @AllArgsConstructor
 @RestController
 @RequestMapping("/api/employees")
